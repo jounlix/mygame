c3b73773d507b93ea09ca2171d70900fa4dee173
jounlix <73626109+jounlix@users.noreply.github.com>
2021-04-10 09:52:22 +0500

    Add files via upload

---
+++ mygame/main.js
@@ -0,0 +1,21 @@
+let cube = {
+    x: 0,
+    y: 0,
+    id: 'cube',
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: 'black'
+}
+
+let map = {
+    x: 0,
+    y: 0,
+    width: 470,
+    height: 470,
+    color: '#f5f5f5'
+}
+
+renderMap(map);
+renderCube(cube);
